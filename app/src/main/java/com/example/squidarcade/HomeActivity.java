cde06997eef29dfc86b141a767e828461dd2e938
Jason Yang <Jasonyang602@gmail.com>
2022-03-21 15:37:13 -0400

    implementing home menu buttons

--- app/src/main/java/com/example/squidarcade/HomeActivity.java
+++ app/src/main/java/com/example/squidarcade/HomeActivity.java
@@ -4,11 +4,32 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+
+import android.widget.Button;
+
+import android.content.Intent;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private Button rlglButtonJ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
+        rlglButtonJ = (Button) findViewById(R.id.rlglButton);
+        rlglButtonJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openRlgl();
+            }
+        });
+
+    }
+    public void openRlgl(){
+        Intent intent = new Intent(this, redLightGreenLight.class);
+        startActivity(intent);
     }
 }
