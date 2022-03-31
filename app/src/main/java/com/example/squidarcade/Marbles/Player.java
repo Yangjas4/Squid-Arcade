53539cb660ab5b8eb2c9d5db495201572ca1e87a
sohbanm <99363017+sohbanm@users.noreply.github.com>
2022-03-30 22:49:21 -0400

    changes

---
+++ app/src/main/java/com/example/squidarcade/Marbles/Player.java
@@ -0,0 +1,32 @@
+package com.example.squidarcade.Marbles;
+
+public class Player {
+    private int marbles;
+    private int gamble;
+
+    public Player(){
+        this.marbles = 10;
+    }
+
+    public int getMarbles(){
+        return this.marbles;
+    }
+
+    public void setGamble(int gamble){
+        this.gamble = gamble;
+    }
+
+    //Have Gamble be a boolean value. True for even and false for odd.
+    //So even button will make gamble Truawe and odd will make it False
+    public void changeMarbles(Boolean gamble){
+        if(gamble){
+            if(marbles % 2 == 0){
+                this.marbles = this.marbles - this.gamble;
+            }else{
+                this.marbles = this.marbles + this.gamble;
+            }
+        }
+    }
+
+
+}
