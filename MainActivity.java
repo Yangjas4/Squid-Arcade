8a1ebf014ac768485572c3460cde909c68977726
TehKevlar <99102487+TehKevlar@users.noreply.github.com>
2022-04-01 16:38:23 -0400

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,29 @@
+package ca.yorku.redlightgreenlight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button singleplayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        singleplayer = (Button) findViewById(R.id.button);
+        singleplayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSingleplayer();
+            }
+        });
+    }
+    public void openSingleplayer(){
+        Intent intent = new Intent(this, Singleplayer.class);
+        startActivity(intent);
+    }
+}
---
+++ Walk.java
@@ -0,0 +1,46 @@
+package ca.yorku.redlightgreenlight;
+
+import static ca.yorku.redlightgreenlight.GameView.screenRatioX;
+import static ca.yorku.redlightgreenlight.GameView.screenRatioY;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import androidx.annotation.CheckResult;
+
+public class Walk {
+    public boolean isGoingForward = false;
+    int x, y, width, height, walkcounter = 0;
+    Bitmap walk1, walk2;
+
+    Walk(int screenX, Resources res){
+        walk1 = BitmapFactory.decodeResource(res, R.drawable.character1);
+        walk2 = BitmapFactory.decodeResource(res, R.drawable.character2);
+
+        width = walk1.getWidth();
+        height = walk1.getHeight();
+
+        width /= 4;
+        height /= 4;
+
+        width *= (int) screenRatioY;
+        height *= (int) screenRatioX;
+
+        walk1 = Bitmap.createScaledBitmap(walk1, width, height, false);
+        walk2 = Bitmap.createScaledBitmap(walk2, width, height, false);
+
+        x = screenX / 2;
+        y = (int) (64 * screenRatioX);
+
+    }
+
+    Bitmap getWalk() {
+        if (walkcounter == 0) {
+            walkcounter++;
+            return walk1;
+        }
+        walkcounter--;
+        return walk2;
+    }
+}
