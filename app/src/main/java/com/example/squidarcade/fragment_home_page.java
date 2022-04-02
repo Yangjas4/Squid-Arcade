8ddf90254bbc3e8a61b8bcf3375ec4861acc7433
Jason Yang <Jasonyang602@gmail.com>
2022-04-02 18:54:31 -0400

    Update fragment_home_page.java

--- app/src/main/java/com/example/squidarcade/fragment_home_page.java
+++ app/src/main/java/com/example/squidarcade/fragment_home_page.java
@@ -23,7 +23,7 @@ public class fragment_home_page extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home_page_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_home_page, container, false);
     }
 
     @Override
