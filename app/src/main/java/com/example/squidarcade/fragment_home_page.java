12dd61e47a6ce211c75d7c8502ce5c1a0ed00969
Jason Yang <Jasonyang602@gmail.com>
2022-04-02 19:54:47 -0400

    marbles home button implementation

--- app/src/main/java/com/example/squidarcade/fragment_home_page.java
+++ app/src/main/java/com/example/squidarcade/fragment_home_page.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,8 +32,12 @@ public class fragment_home_page extends Fragment implements View.OnClickListener
         return homePage;
     }
 
-    public void onClick(View view){
-        
+    public void onClick(View view) {
+        if (view == marblesBtn) {
+            NavHostFragment.findNavController(fragment_home_page.this).navigate(R.id.action_fragment_home_page_to_fragment_marbles_main);
+        } else {
+            //TODO: implement rlgl fragments later
+            NavHostFragment.findNavController(fragment_home_page.this).navigate(R.id.action_fragment_home_page_to_fragment_marbles_main);
+        }
     }
-
 }
