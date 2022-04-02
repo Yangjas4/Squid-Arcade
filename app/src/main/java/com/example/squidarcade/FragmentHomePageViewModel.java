383e9ea0852c1d30a80702363d791d3b7febc4e6
Jason Yang <Jasonyang602@gmail.com>
2022-04-02 18:45:41 -0400

    navigation

---
+++ app/src/main/java/com/example/squidarcade/FragmentHomePageViewModel.java
@@ -0,0 +1,7 @@
+package com.example.squidarcade;
+
+import androidx.lifecycle.ViewModel;
+
+public class FragmentHomePageViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/squidarcade/fragment_home_page.java
@@ -0,0 +1,36 @@
+package com.example.squidarcade;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragment_home_page extends Fragment {
+
+    private FragmentHomePageViewModel mViewModel;
+
+    public static fragment_home_page newInstance() {
+        return new fragment_home_page();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home_page_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(FragmentHomePageViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
