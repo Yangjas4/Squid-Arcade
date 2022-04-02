419496e9e6cc9490232ad83fb77310883858d793
TehKevlar <aman1703@my.yorku.ca>
2022-04-02 18:16:45 -0400

    Activity mainrlgl error

--- app/src/main/java/com/example/squidarcade/MainActivitySingleplayer.java
+++ app/src/main/java/com/example/squidarcade/MainActivitySingleplayer.java
@@ -7,13 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivitySingleplayer extends AppCompatActivity {
     private Button singleplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_MainRLGL);
         singleplayer = (Button) findViewById(R.id.button);
         singleplayer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public void openSingleplayer(){
-        Intent intent = new Intent(this, Singleplayer.class);
+        Intent intent = new Intent(MainActivitySingleplayer.this, Singleplayer.class);
         startActivity(intent);
     }
 }
