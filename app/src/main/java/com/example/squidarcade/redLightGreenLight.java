5b6ec429ae318ac42b1d281f8f8814266460cbc7
sohbanm <99363017+sohbanm@users.noreply.github.com>
2022-04-02 14:26:24 -0400

    1

--- app/src/main/java/com/example/squidarcade/redLightGreenLight.java
+++ app/src/main/java/com/example/squidarcade/redLightGreenLight.java
@@ -9,6 +9,6 @@ public class redLightGreenLight extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.rlglPlayers);
+        setContentView(R.layout.rlgl_players);
     }
 }
