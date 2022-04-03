868ad9dfced515425b6f8b84329a18b6536ed9e1
Jason Yang <Jasonyang602@gmail.com>
2022-04-02 23:18:17 -0400

    awd

--- app/src/main/java/com/example/squidarcade/redLightGreenLight.java
+++ app/src/main/java/com/example/squidarcade/redLightGreenLight.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RLGLMain extends AppCompatActivity {
+public class redLightGreenLight extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rlglmain);
+        setContentView(R.layout.rlgl_players);
     }
 }
