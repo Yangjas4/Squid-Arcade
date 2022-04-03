50887f68b304449ceab5318e7d285199321b1d1c
Jason Yang <Jasonyang602@gmail.com>
2022-04-02 21:37:06 -0400

    ADWAW

--- app/src/main/java/com/example/squidarcade/fragment_marbles_main.java
+++ app/src/main/java/com/example/squidarcade/fragment_marbles_main.java
@@ -3,12 +3,13 @@ package com.example.squidarcade;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-<<<<<<< HEAD
 import com.example.squidarcade.Marbles.Singleplayer.singleGame;
 
 public class fragment_marbles_main extends Fragment implements View.OnClickListener {
